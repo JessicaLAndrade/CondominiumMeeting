2bf36167a04befb12df2c5096ae8baabe9483d6c
mauroph1 <122278388+mauroph1@users.noreply.github.com>
2023-06-21 16:55:37 -0300

    tela de criar formulario

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,126 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:sindical_meet/voting_screen.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+      title: 'Criar Google Form',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CreateFormPage(),
+    );
+  }
+}
+
+class CreateFormPage extends StatefulWidget {
+  const CreateFormPage({Key? key}) : super(key: key);
+
+  @override
+  _CreateFormPageState createState() => _CreateFormPageState();
+}
+
+class _CreateFormPageState extends State<CreateFormPage> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _questionController = TextEditingController();
+
+  String _message = '';
+
+  Future<void> createForm() async {
+    const url = 'https://forms.googleapis.com/forms/v1/forms';
+    const apiKey = 'AIzaSyA9HkozS8vUzrJTCt-ME5_JMCBVbrg3j8k';
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $apiKey',
+      },
+      body: json.encode({
+        'title': _titleController.text,
+        'questions': [
+          {
+            'question': {
+              'title': _questionController.text,
+              'required': true,
+            },
+            'index': 0,
+          },
+        ],
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      final formId = responseData['formId'];
+
+      setState(() {
+        _message = 'Formulário criado com sucesso! ID do formulário: $formId';
+      });
+    } else {
+      setState(() {
+        _message =
+            'Erro ao criar o formulário. Código de status: ${response.statusCode}';
+      });
+    }
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Status do Formulário'),
+          content: Text(_message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Fechar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Criar Google Form'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration:
+                  const InputDecoration(labelText: 'Título do Formulário'),
+            ),
+            TextField(
+              controller: _questionController,
+              decoration: const InputDecoration(labelText: 'Pergunta'),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                createForm();
+              },
+              child: const Text('Criar Formulário'),
+            ),
+          ],
         ),
-        home: const VotingScreen());
+      ),
+    );
   }
 }
--- lib/voting_screen.dart
+++ lib/voting_screen.dart
@@ -1,39 +1,97 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
 
-class VotingScreen extends StatelessWidget {
-  final String googleFormsUrl =
-      'https://docs.google.com/forms/d/e/1FAIpQLSdxhCbeWK8ONnkIVHv2v4B968aEBybpkUtQYSLBLhoJFcx9iQ/viewform';
+class VotingPage extends StatefulWidget {
+  const VotingPage({super.key});
 
-  const VotingScreen({super.key}); // URL do Google Forms
+  @override
+  _VotingPageState createState() => _VotingPageState();
+}
 
-  void launchGoogleForms() async {
-    if (await canLaunch(googleFormsUrl)) {
-      await launch(googleFormsUrl);
-    } else {
-      throw 'Não foi possível abrir o Google Forms';
+class _VotingPageState extends State<VotingPage> {
+  final TextEditingController _idController = TextEditingController();
+  final TextEditingController _meetingController = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _optionsController = TextEditingController();
+
+  void sendEmails() async {
+    // Configurações do servidor SMTP
+    final smtpServer = SmtpServer('smtp.example.com',
+        username: 'seu-email@example.com', password: 'sua-senha');
+
+    // Configurações do e-mail
+    final message = Message()
+      ..from = const Address('seu-email@example.com')
+      ..subject = 'Lembrete: Responda à enquete'
+      ..text =
+          'Caro destinatário,\n\nPor favor, lembre-se de responder à enquete.\n\nLink para a enquete: [INSIRA O LINK DO GOOGLE FORMS AQUI]'
+      ..recipients.addAll([
+        'email1@example.com',
+        'email2@example.com',
+        'email3@example.com',
+      ]);
+
+    try {
+      final sendReport = await send(message, smtpServer);
+      print(
+          'E-mail enviado com sucesso! Resposta: ${sendReport.messageSendingEnd}');
+    } catch (e) {
+      print('Erro ao enviar e-mail: $e');
     }
+  }
+
+  void sendReminderEmails() {
+    // Chame essa função diariamente para enviar lembretes por e-mail
+    sendEmails();
+  }
+
+  void sendAuthorizationEmails() {
+    // Chame essa função no dia anterior ao encerramento da enquete para enviar e-mails solicitando autorização
+    sendEmails();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Votação'),
+        title: const Text('Criar Enquete'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'Por favor, clique no botão abaixo para acessar a votação:',
-              style: TextStyle(fontSize: 18),
+            TextField(
+              controller: _idController,
+              decoration: const InputDecoration(labelText: 'ID'),
             ),
-            const SizedBox(height: 16),
+            TextField(
+              controller: _meetingController,
+              decoration: const InputDecoration(labelText: 'Reunião'),
+            ),
+            TextField(
+              controller: _titleController,
+              decoration: const InputDecoration(labelText: 'Título'),
+            ),
+            TextField(
+              controller: _optionsController,
+              decoration: const InputDecoration(labelText: 'Opções'),
+            ),
+            const SizedBox(height: 16.0),
             ElevatedButton(
-              onPressed: launchGoogleForms,
-              child: const Text('Acessar Votação'),
+              onPressed: () {
+                // Criar a enquete no Google Forms usando as informações fornecidas
+                // e obtenha o link do formulário
+                String googleFormsLink = '[INSIRA O LINK DO GOOGLE FORMS AQUI]';
+
+                // Enviar e-mails diários de lembrete
+                sendReminderEmails();
+
+                // Enviar e-mail de autorização no dia anterior ao encerramento da enquete
+                sendAuthorizationEmails();
+              },
+              child: const Text('Criar Enquete'),
             ),
           ],
         ),
